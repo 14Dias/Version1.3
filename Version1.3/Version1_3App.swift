1c3aa0dd622e9c562e3958ee09e54a51533e5a30
luca <Lucadiasferreira@gmail.com>
2025-11-09 21:44:52 -0300

    Initial Commit

---
+++ Version1.3/Version1_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Version1_3App.swift
+//  Version1.3
+//
+//  Created by Luca on 09/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Version1_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Version1.3Tests/Version1_3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Version1_3Tests.swift
+//  Version1.3Tests
+//
+//  Created by Luca on 09/11/25.
+//
+
+import Testing
+@testable import Version1_3
+
+struct Version1_3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
